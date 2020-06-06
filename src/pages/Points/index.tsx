ffc87cc85b4415787523ecccf6fa50e9a7b796d1
Micaelly Uchoa <micaellyit@hotmail.com>
2020-06-06 16:10:37 -0300

    feature: tela de point

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { View, TouchableOpacity, Text, ScrollView } from "react-native";
+import { View, TouchableOpacity, Text, ScrollView, Image } from "react-native";
 import styles from "./styles";
 import { Feather as Icon } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { SvgUri } from "react-native-svg";
 const Points = () => {
   const navigation = useNavigation();
   function handlerNavigateBack() {
     navigation.goBack();
+  }
+  function handleNavigateToDetail() {
+    navigation.navigate("Detail");
   }
   return (
     <>
@@ -22,7 +25,35 @@ const Points = () => {
           Encontre no mapa um ponto de coleta.
         </Text>
         <View style={styles.mapContainer}>
-          <MapView style={styles.map} />
+          <MapView
+            initialRegion={{
+              latitude: -23.5746863,
+              longitude: -46.6285515,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+            style={styles.map}
+          >
+            <Marker
+              style={styles.mapMarker}
+              onPress={handleNavigateToDetail}
+              coordinate={{
+                latitude: -23.5746863,
+                longitude: -46.6285515,
+              }}
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image
+                  style={styles.mapMarkerImage}
+                  source={{
+                    uri:
+                      "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+                  }}
+                />
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+            </Marker>
+          </MapView>
         </View>
       </View>
       <View style={styles.itemsContainer}>
